72b35ff99c4f94a903e2676d56beb63e63401ae0
Niraj <nirajbalwani766@gmail.com>
2023-03-12 23:46:27 +0530

    Android App

---
+++ src/main/java/com/example/news/ui/slideshow/SlideshowFragment.java
@@ -0,0 +1,37 @@
+package com.example.news.ui.slideshow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.news.R;
+import com.example.news.WebViewController;
+import com.example.news.databinding.FragmentSlideshowBinding;
+
+public class SlideshowFragment extends Fragment {
+
+    private FragmentSlideshowBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SlideshowViewModel slideshowViewModel =
+                new ViewModelProvider(this).get(SlideshowViewModel.class);
+
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        WebView webView = root.findViewById(R.id.webViewBBCCC);
+        webView.loadUrl("https://www.bbc.com/");
+        webView.setWebViewClient(new WebViewController());
+
+        return root;
+    }
+
+}
